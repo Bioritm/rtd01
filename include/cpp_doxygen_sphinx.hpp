599ec55edecaa92ae30493a4309edd1525080068
Bioritm <bioritmixer@ya.ru>
2024-03-27 22:55:36 +0300

    23

--- include/cpp_doxygen_sphinx.hpp
+++ include/cpp_doxygen_sphinx.hpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
 
-/// Foo
+/// Класс для демонстрации вывода сообщения
 class Foo {
-
-    /// Say hello
-    /// @param message The message to print
-    void say_hello(std::string message) const;
-
+public:
+    /// Выводит приветствие с сообщением
+    /// @param message Сообщение для вывода
+    void say_hello(const std::string& message) const {
+        std::cout << message << std::endl;
+    }
 };
+
+int main() {
+    Foo foo;
+    foo.say_hello("Hello, World!");  // Вызов метода say_hello объекта класса Foo
+    return 0;
+}
--- src/cpp_doxygen_sphinx.cpp
+++ src/cpp_doxygen_sphinx.cpp
@@ -1,5 +1,6 @@
 #include "../include/cpp_doxygen_sphinx.hpp"
+#include <iostream>
 
-void Foo::say_hello(std::string message) const {
+void Foo::say_hello(const std::string& message) const {
   std::cout << "Hello: " << message << std::endl;
 }
